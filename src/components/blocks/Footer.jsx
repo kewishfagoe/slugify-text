602b8c55e7a1873fcf8a50576bc8dc559425e74d
Kewish Fagoe <fagoe.kewish@gmail.com>
2025-02-16 13:14:59 -0300

    Added footer component

---
+++ src/components/blocks/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="p-5 mt-15 border-t-2 border-slate-200">
+        <p>Made with ♡ by Kewish</p>
+        <a className="text-primary" href="https://github.com/kewishfagoe" rel="noopener noreferrer">GitHub &#8599;</a>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Header from '@components/blocks/Header'
 import { APP_NAME } from '@utils/constants/globals.constants'
+import Header from '@components/blocks/Header'
+import Footer from '@components/blocks/Footer'
 
 const HomePage = () => {
   return (
     <>
         <Header text={APP_NAME} />
+        <Footer />
     </>
   )
 }
