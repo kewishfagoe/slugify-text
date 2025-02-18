14b8f01b5ebae950229257d9f9385daebfe7eb9b
Kewish Fagoe <fagoe.kewish@gmail.com>
2025-02-18 16:54:56 -0300

    Update button component styles

--- src/components/forms/Button/Button.jsx
+++ src/components/forms/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ onClick, children, className }) => {
 
-    const defaultClassNames = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800";
+    const defaultClassNames = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none cursor-pointer";
 
     const combinedClassNames = className ? `${defaultClassNames} ${className}` : defaultClassNames;
 
