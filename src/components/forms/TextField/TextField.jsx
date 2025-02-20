5a7d0a365e7816e2d89ba4d74d9441148f2ecc46
Kewish Fagoe <fagoe.kewish@gmail.com>
2025-02-20 19:49:08 -0300

    Update textfield component

--- src/components/forms/TextField/TextField.jsx
+++ src/components/forms/TextField/TextField.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 
-const TextField = ({ onChange, className, placeholder }) => {
+const TextField = ({ onChange, className, placeholder, id, label }) => {
     const defaultClassNames = "block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500"
     const combinedClassNames = className ? `${defaultClassNames} ${className}` : defaultClassNames
 
-    const defaultPlaceholder = "Enter text"
+    const defaultPlaceholder = "Enter text..."
     const assignedPlaceholder = placeholder ? placeholder : defaultPlaceholder
+
+    const defaultLabel = "Enter text:"
+    const assignedLabel = label ? label : defaultLabel
 
     const [value, setValue] = useState("")
 
@@ -15,13 +18,24 @@ const TextField = ({ onChange, className, placeholder }) => {
     }
 
     return (
-        <input
-            type="text"
-            value={value}
-            onChange={handleChange}
-            placeholder={assignedPlaceholder}
-            className={combinedClassNames}
-        />
+        <>
+            {label && (
+                <label
+                    htmlFor={id}
+                    className="block mb-2"
+                >
+                    {assignedLabel}
+                </label>
+            )}
+            <input
+                type="text"
+                value={value}
+                onChange={handleChange}
+                placeholder={assignedPlaceholder}
+                className={combinedClassNames}
+                id={id}
+            />
+        </>
     )
 }
 
