f3121cf9ce4e33e33956902cb5e68843d610a7df
Kewish Fagoe <fagoe.kewish@gmail.com>
2025-02-23 14:19:56 -0300

    Lift textfield component state up to parent component to easily clear textfield value

--- src/components/forms/TextField/TextField.jsx
+++ src/components/forms/TextField/TextField.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const TextField = ({ onChange, className, placeholder, id, label }) => {
+const TextField = ({ value, onChange, className, placeholder, id, label }) => {
     const defaultClassNames = "block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500"
     const combinedClassNames = className ? `${defaultClassNames} ${className}` : defaultClassNames
 
@@ -10,10 +10,7 @@ const TextField = ({ onChange, className, placeholder, id, label }) => {
     const defaultLabel = "Enter text:"
     const assignedLabel = label ? label : defaultLabel
 
-    const [value, setValue] = useState("")
-
     const handleChange = (e) => {
-        setValue(e.target.value)
         onChange(e.target.value)
     }
 
